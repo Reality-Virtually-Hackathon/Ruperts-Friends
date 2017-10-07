fca2e8fd1fe0b638ac730d14fe4a4bfe86a44175
tusadi <tpurang@yahoo.in>
2017-10-07 11:48:37 -0400

    11:47am

---
+++ TeddyBearVR_Maxime/Assets/Scripts/ragdollTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ragdollTest : MonoBehaviour {
+
+	public Animator ragdoll;
+	public bool state = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			ragdoll.SetBool("switch",!state);
+			state = !state; 
+		}
+	}
+}
